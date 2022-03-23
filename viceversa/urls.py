9b264bb04769810e46b44081601476912e9b3ed3
Oleksandrz Zaharenko <you@example.com>
2022-03-23 07:29:10 +0100

    Origin and reversed text

--- viceversa/urls.py
+++ viceversa/urls.py
@@ -18,5 +18,6 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('home/', views.home),
+    path('', views.home),
+    path('new_page/', views.new_page, name = "new_page")
 ]
--- viceversa/views.py
+++ viceversa/views.py
@@ -2,3 +2,10 @@ from django.shortcuts import render
 
 def home(request):
 	return render(request, 'home.html')
+
+
+def new_page(request):
+	userText = request.GET['usertext']
+	reversedText = userText[::-1]
+	return render(request, 'new_page.html', {'usertext': userText, 
+											'reversedtext':reversedText} )
